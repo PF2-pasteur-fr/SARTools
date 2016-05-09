f563e5791b5c6f3bb4e510cb59fd5a3fd671e8bf
hvaret <hugo.varet@pasteur.fr>
2016-05-09 15:25:32 +0200

    1.3.0

--- R/NAMESPACE.R
+++ R/NAMESPACE.R
@@ -2,9 +2,13 @@
 #' @import DESeq2
 #' @import edgeR
 #' @import xtable
+#' @importFrom grDevices col2rgb dev.off png
+#' @importFrom graphics abline barplot boxplot curve hist legend lines pairs par plot points text
+#' @importFrom stats density dist dnorm formula hclust lm model.matrix p.adjust.methods prcomp quantile relevel sd var
+#' @importFrom utils combn head installed.packages packageVersion read.table tail write.table
 #' @importFrom knitr knit2html
-#' @importFrom GenomicRanges colData assay
-#' @importFrom S4Vectors mcols
+#' @importFrom SummarizedExperiment assay colData
+#' @importFrom S4Vectors mcols metadata
 #' @importFrom limma plotMDS
 #' @importFrom genefilter shorth
 NULL
