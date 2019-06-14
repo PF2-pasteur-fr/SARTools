5dbaa626d1c89f917750a9852b00eadfea202924
hvaret <hugo.varet@pasteur.fr>
2019-06-14 12:25:17 +0200

    vignetteAndDupIds

--- template_script_DESeq2.r
+++ template_script_DESeq2.r
@@ -2,7 +2,7 @@
 ### R script to compare several conditions with the SARTools and DESeq2 packages
 ### Hugo Varet
 ### March 20th, 2018
-### designed to be executed with SARTools 1.6.7
+### designed to be executed with SARTools 1.6.9
 ################################################################################
 
 ################################################################################
